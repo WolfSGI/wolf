f28a451a8d68342808427948f5df05c3bb221dd4
Souheil Chelfouh <trollfot@gmail.com>
2026-05-14 14:46:01 +0200

    Added tests for request registry lookup

--- tests/app/test_request.py
+++ tests/app/test_request.py
@@ -1,8 +1,14 @@
 import pytest
+from svcs import Registry
 from webtest.app import TestRequest as EnvironBuilder
 from kettu.datastructures import Data
-from kettu.headers import Query, Authorization, DigestAuthParams
-from wolf.app.request import Request
+from kettu.headers import Cookies, Query, Authorization, DigestAuthParams
+from wolf.app.request import Request, ServiceNotFoundError
+from wolf.app.response import Response
+
+
+class MockService:
+    pass
 
 
 def test_request():
@@ -56,3 +62,37 @@ def test_request_authorization():
             uri="/?a=b"
         )
     )
+
+
+def test_request_response_class():
+    environ = EnvironBuilder.blank('/', method='GET').environ
+    request = Request(environ)
+    assert request.response_cls is Response
+
+    class MyResponse(Response):
+        ...
+
+    request = Request(environ, response_cls=MyResponse)
+    assert request.response_cls is MyResponse
+
+
+def test_blank_context():
+    environ = EnvironBuilder.blank('/', method='GET').environ
+    request = Request(environ)
+
+    with pytest.raises(NotImplementedError):
+        request.get(Cookies)
+
+    context = Registry()
+    with request(context):
+        assert request.get(Cookies) is request.cookies
+        assert request.get(Query) is request.query
+        assert request.get(Request) is request
+
+        data = request.get(Data)
+        assert isinstance(data, Data)
+
+        with pytest.raises(ServiceNotFoundError):
+            assert request.get(MockService)
+
+        assert request.get(MockService, default=None) is None
